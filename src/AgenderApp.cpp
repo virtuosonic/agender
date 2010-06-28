8d3bd0b3403c4a8d1f65021adfd2d361327a8dbd
virtuosonic <22582422+virtuosonic@users.noreply.github.com>
2010-06-28 15:22:53 +0000

    rearranged project tree

---
+++ src/AgenderApp.cpp
@@ -0,0 +1,154 @@
+/***************************************************************
+ * Name:      AgenderApp.cpp
+ * Purpose:   Code for Application Class
+ * Author:    Gabriel Espinoza
+ * Created:   2008-11-21
+ * Copyright: Gabriel Espinoza
+ * License: GPLv3+
+ **************************************************************/
+
+#include "AgenderApp.h"
+#include "AgenderMain.h"
+#include "AgenderIPC.h"
+
+#include <wx/log.h>
+#include <wx/defs.h>
+#include <wx/stdpaths.h>
+#include <wx/cmdline.h>
+#include <wx/apptrait.h>
+#include <iostream>
+
+#if defined __UNIX__
+#include <signal.h>
+//i hate globals
+void OnSignal(int sig);
+#endif
+
+//(*AppHeaders
+#include <wx/image.h>
+//*)
+
+BEGIN_EVENT_TABLE(AgenderApp,wxApp)
+	EVT_QUERY_END_SESSION(AgenderApp::OnEndSession)
+	EVT_END_SESSION(AgenderApp::OnEndSession)
+END_EVENT_TABLE()
+
+IMPLEMENT_APP(AgenderApp);
+
+bool AgenderApp::OnInit()
+{
+	//who are we?
+	SetAppName(_T("Agender"));
+	SetVendorName(_T("Virtuosonic"));
+	//redirect logging to cout, if you want to annoy users use ::wxMessageBox(_T("do you want me to annoy you?"));
+	delete wxLog::SetActiveTarget(new wxLogStream(&std::cout));
+	//parse arguments
+	wxCmdLineParser cmd(argc,argv);
+	cmd.AddOption(_T("c"),_T("config"),_T("specify a config file to load"),wxCMD_LINE_VAL_STRING);
+	cmd.AddSwitch(_T("nt"),_T("no-taskbar"),_T("use when you don't have a taskbar"));
+	OnInitCmdLine(cmd);
+	int res = cmd.Parse(false);
+	if (res < 0)
+	{
+		cmd.Usage();
+		exit(EXIT_SUCCESS);
+	}
+	wxString cfgFile;
+	cmd.Found(_T("c"),&cfgFile);
+	//are we alone?
+	m_checker = new wxSingleInstanceChecker;
+	if (m_checker->Create(_T(".") + GetAppName() + _T("-") + ::wxGetUserId())
+		&& m_checker->IsAnotherRunning())
+	{
+		//lets try to connect to Another and  asking to show it self
+		wxClient client;
+		wxConnection* cnn = NULL;
+		cnn = (wxConnection*)client.MakeConnection(_T("localhost"),IPC_Service,IPC_Topic);
+		if (cnn)
+		{
+			wxLogMessage(_T("executing"));
+			if (cnn->Execute(NULL))
+			{
+				wxLogMessage(_T("finished executing"));
+				//first ending, like on video games it sucks!
+				exit(EXIT_SUCCESS);
+			}
+			wxLogError(_T("not executed"));
+		}
+		else
+			wxLogError(_T("connection failed: %s"),wxSysErrorMsg());
+		//this goes outside of the 'else' because  if everything goes right : exit(EXIT_SUCCESS);
+		//second ending, like on videogames it sucks even more!
+		exit(EXIT_FAILURE);
+	}
+	// please talk me in a language that i understand
+	if (m_locale.Init(wxLANGUAGE_DEFAULT,wxLOCALE_CONV_ENCODING))
+		m_locale.AddCatalog(wxT("Agender"));
+	//(*AppInitialize
+	bool wxsOK = true;
+	wxInitAllImageHandlers();
+	//*)
+	//create main frame
+	wxFrame* Frame = new AgenderFrame(m_locale,cfgFile);
+	SetTopWindow(Frame);
+	//lets create a server so Anothers can comunicate with this->m_server
+	#ifndef __WXMSW__
+	m_server = new AgenderServer;
+	if (m_server->Create(IPC_Service))
+		wxLogMessage(_T("server created"));
+	else
+		wxLogError(_T("server creation failed"));
+	#else
+	m_server = NULL;
+	#endif
+	//no taskbar?
+	if (cmd.Found(_T("nt")))
+		Frame->Show();
+	#ifdef __UNIX__
+	signal(SIGINT,&OnSignal);
+	signal(SIGTERM,&OnSignal);
+	#endif
+	#if !defined wxHAS_TASK_BAR_ICON
+	Frame->Show();
+	// TODO (virtuoso#1#): think in another way to exit agender
+	//without having to kill it
+	//this is intended for some mobile platform, is it needed?
+	//i remember that windows CE had a function to finish apps
+	// and accoding to its guidelines it should continue in memory
+	//but what about Familiar Linux, iPhoneOS, etc ?
+	#endif//wxHAS_TASK_BAR_ICON
+	return wxsOK;
+}
+
+int AgenderApp::OnExit()
+{
+	if (m_checker)
+		delete m_checker;
+	if (m_server)
+		delete m_server;
+	wxLogMessage(_T("Exiting: goodbye"));
+	return wxApp::OnExit();
+}
+
+#ifdef __UNIX__
+void OnSignal(int sig)
+{
+	wxLogMessage(_T("signal %i catched"),sig);
+	if (wxTheApp->GetTopWindow())
+	{
+		wxTheApp->GetTopWindow()->Show();
+		wxTheApp->GetTopWindow()->Destroy();
+	}
+}
+#endif
+
+void AgenderApp::OnEndSession(wxCloseEvent& event)
+{
+	wxLogMessage(_T("ending session"));
+	if (GetTopWindow())
+	{
+		GetTopWindow()->Show();
+		GetTopWindow()->Destroy();
+	}
+}
+
---
+++ src/AgenderTray.cpp
@@ -0,0 +1,192 @@
+/***************************************************************
+ * Name:      AgenderTray.cpp
+ * Purpose:   Code for the tray/notification/taskbar/etc icon
+ * Author:    Gabriel Espinoza
+ * Copyright: Gabriel Espinoza
+ * License: GPLv3+
+ **************************************************************/
+#include <wx/defs.h>
+
+#if defined wxHAS_TASK_BAR_ICON
+
+#include "AgenderTray.h"
+#include <wx/colordlg.h>
+#include <wx/menu.h>
+#include <wx/config.h>
+#include <wx/app.h>
+#include <wx/clipbrd.h>
+#include <wx/dataobj.h>
+#if wxUSE_RICHTEXT
+#include <wx/richtext/richtextsymboldlg.h>
+#endif
+
+BEGIN_EVENT_TABLE(AgenderTray,wxTaskBarIcon)
+	EVT_TASKBAR_LEFT_UP(AgenderTray::OnLeft)
+	EVT_MENU(ID_SHOW,AgenderTray::OnMenuShow)
+	EVT_MENU(ID_HIDE,AgenderTray::OnMenuHide)
+	EVT_MENU_RANGE(ID_OPC100,ID_OPC25,AgenderTray::OnMenuOpc)
+	EVT_MENU(ID_YEARSEL,AgenderTray::OnYearSel)
+	EVT_MENU(ID_NOTES_COLOUR,AgenderTray::OnMenuNotesColour)
+	EVT_MENU(wxID_EXIT,AgenderTray::OnMenuExit)
+	EVT_MENU(wxID_FIND,AgenderTray::OnMenuFind)
+	EVT_MENU(ID_AUTOSTART,AgenderTray::OnMenuAutoStart)
+	EVT_MENU(ID_SYMBOL,AgenderTray::OnMenuSymbols)
+END_EVENT_TABLE()
+
+AgenderTray::AgenderTray(wxFrame* frame,long colalpha)
+{
+	this->frame = frame;
+	alpha = colalpha;
+	switch (alpha)
+	{
+		case 255:
+			opc = ID_OPC100;
+			break;
+		case 187:
+			opc = ID_OPC75;
+			break;
+		case 125:
+			opc = ID_OPC50;
+			break;
+		case 62:
+			opc = ID_OPC25;
+			break;
+
+		default:
+			break;
+	}
+}
+
+void AgenderTray::OnLeft(wxTaskBarIconEvent& event)
+{
+	if (frame->IsShownOnScreen())
+	{
+		frame->Hide();
+	}
+	else
+	{
+		frame->Show();
+		//frame->SetFocus();
+		//frame->Raise();
+	}
+}
+
+wxMenu * AgenderTray::CreatePopupMenu()
+{
+	wxMenu* opcMenu;
+	opcMenu = new wxMenu;
+	opcMenu->AppendRadioItem(ID_OPC100,_("100%"));
+	opcMenu->AppendRadioItem(ID_OPC75,_("75%"));
+	opcMenu->AppendRadioItem(ID_OPC50,_("50%"));
+	opcMenu->AppendRadioItem(ID_OPC25,_("25%"));
+	opcMenu->Check(opc,true);
+	//
+	wxMenu* menu;
+	menu = new wxMenu;
+	menu->Append(ID_SHOW,_("Show"));
+	menu->Append(ID_HIDE,_("Hide"));
+	menu->AppendSeparator();
+	if (frame->CanSetTransparent())
+		menu->AppendSubMenu(opcMenu,_("Opacity"));
+	menu->AppendCheckItem(ID_YEARSEL,_("Year selector"));
+	menu->Append(ID_NOTES_COLOUR,_("Notes Colour"));
+	#if wxUSE_RICHTEXT
+	menu->Append(ID_SYMBOL,_("Symbol"));
+	#endif
+	menu->AppendCheckItem(ID_AUTOSTART,_("Autostart"));
+	menu->AppendSeparator();
+	menu->Append(wxID_EXIT,_("Exit"));
+	//read value from config
+	bool test_bool= false;
+	wxConfig::Get()->Read(_T("/yearselector"),&test_bool);
+	menu->Check(ID_YEARSEL,test_bool);
+	wxConfig::Get()->Read(_T("/autostart"),&test_bool);
+	menu->Check(ID_AUTOSTART,test_bool);
+
+	return menu;
+}
+
+void AgenderTray::OnMenuExit(wxCommandEvent& event)
+{
+	frame->Destroy();
+}
+
+void AgenderTray::OnMenuHide(wxCommandEvent& event)
+{
+	frame->Hide();
+}
+
+void AgenderTray::OnMenuShow(wxCommandEvent& event)
+{
+	frame->Show();
+	frame->SetFocus();
+	frame->Raise();
+}
+
+void AgenderTray::OnMenuOpc(wxCommandEvent& event)
+{
+	opc = event.GetId();
+	switch (opc)
+	{
+		case ID_OPC25:
+			alpha = 62;
+			break;
+		case ID_OPC50:
+			alpha = 125;
+			break;
+		case ID_OPC75:
+			alpha = 187;
+			break;
+		default:
+			alpha = 255;
+			break;
+	}
+	frame->SetTransparent(alpha);
+	wxConfig::Get()->Write(_T("/opacity"),alpha);
+}
+
+void AgenderTray::OnMenuNotesColour(wxCommandEvent& event)
+{
+	wxColourDialog dlg(frame);
+	dlg.GetColourData().SetChooseFull(true);
+	if (dlg.ShowModal() == wxID_OK)
+	{
+		event.SetString(dlg.GetColourData().GetColour().GetAsString(wxC2S_HTML_SYNTAX));
+		::wxPostEvent(frame->GetEventHandler(),event);
+	}
+}
+
+void AgenderTray::OnMenuFind(wxCommandEvent& event)
+{
+	wxPostEvent(frame->GetEventHandler(),event);
+}
+
+void AgenderTray::OnYearSel(wxCommandEvent& event)
+{
+	wxConfig::Get()->Write(_T("/yearselector"),event.IsChecked());
+	wxPostEvent(frame->GetEventHandler(),event);
+}
+
+void AgenderTray::OnMenuAutoStart(wxCommandEvent& event)
+{
+	wxConfig::Get()->Write(_T("/autostart"),event.IsChecked());
+	wxPostEvent(frame->GetEventHandler(),event);
+}
+
+void AgenderTray::OnMenuSymbols(wxCommandEvent& event)
+{
+	#if wxUSE_RICHTEXT
+	wxSymbolPickerDialog dlg(_T("*"),wxEmptyString,
+					 wxTheApp->GetTopWindow()->GetFont().GetFaceName(),NULL);
+	if (dlg.ShowModal() ==wxID_OK && dlg.HasSelection())
+	{
+		if (wxTheClipboard->Open())
+		{
+			wxTheClipboard->SetData(new wxTextDataObject(dlg.GetSymbol()));
+			wxTheClipboard->Close();
+		}
+	}
+	#endif
+}
+
+#endif //wxHAS_TASK_BAR_ICON
