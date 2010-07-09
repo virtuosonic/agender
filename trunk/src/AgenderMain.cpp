cf668586ea73f752b56d6357b6dbfd5483800609
virtuosonic <virtuosonic@f8c76fb2-cb6b-4cb0-a8c3-d6e765d22dc4>
2010-07-09 18:43:10 +0000

    added fhaag to translators in credits

--- trunk/src/AgenderMain.cpp
+++ trunk/src/AgenderMain.cpp
@@ -238,9 +238,10 @@ void AgenderFrame::OnButton3Click(wxCommandEvent& event)
 {
 	wxAboutDialogInfo info;
 	info.AddDeveloper(_T("Gabriel Espinoza <virtuosonic@users.sourceforge.net>"));
-	info.AddTranslator(_T("Gabriel Espinoza <español>"));
-	info.AddTranslator(_T("Ester Espinoza <deutsch>"));
-	info.AddTranslator(_T("Daniel Daows <japanese>"));
+	info.AddTranslator(_T("Gabriel Espinoza : español"));
+	info.AddTranslator(_T("Ester Espinoza : deutsch"));
+	info.AddTranslator(_T("Florian Haag : deutsch"));
+	info.AddTranslator(_T("Daniel Daows : japanese"));
 	info.SetDescription(wxString::Format(_T("%s\n%s %s %s"),_("A cross-platform schedule tool"),
 							 _("Build:"),__TDATE__,__TTIME__));
 	info.SetWebSite(_T("http://agender.sourceforge.net"),_("Agender Web Site"));
