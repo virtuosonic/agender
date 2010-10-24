b6cb21b67cd059bad27ba7c9279e4c456c26a1ac
virtuosonic <virtuosonic@f8c76fb2-cb6b-4cb0-a8c3-d6e765d22dc4>
2010-10-24 04:54:05 +0000

    tagged 1.1.8.1 bug fix

---
+++ tags/1.1.8.1/src/AgenderApp.h
@@ -0,0 +1,34 @@
+/***************************************************************
+ * Name:      AgenderApp.h
+ * Purpose:   Defines Application Class
+ * Author:    Gabriel Espinoza (virtuosonic@users.sourceforge.net)
+ * Created:   2008-11-21
+ * Copyright: Gabriel Espinoza
+ * License: GPLv3+
+ **************************************************************/
+#ifndef AGENDERAPP_H_INCLUDED
+#define AGENDERAPP_H_INCLUDED
+
+#include <wx/app.h>
+#include <wx/snglinst.h>
+#include <wx/intl.h>
+#include <wx/ipc.h>
+#include "Notifier.h"
+//#include "AgenderIPC.h"
+
+class AgenderApp : public wxApp
+{
+	public:
+		bool OnInit();
+		int OnRun();
+		int OnExit();
+	private:
+		void OnEndSession(wxCloseEvent& event);
+		wxSingleInstanceChecker* m_checker;
+		wxServer* m_server;
+		wxLocale m_locale;
+		Notifier notif;
+		DECLARE_EVENT_TABLE()
+};
+
+#endif // AGENDERAPP_H_INCLUDED
