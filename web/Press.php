6384deb061f523597ef55c2a259f6e157e3ca743
virtuosonic <virtuosonic@f8c76fb2-cb6b-4cb0-a8c3-d6e765d22dc4>
2010-10-25 06:50:55 +0000

    eliminados algunos <br> redundantes

--- web/Press.php
+++ web/Press.php
@@ -7,16 +7,13 @@ Note: this reviews can be old
 <ul>
 <li>
 	<a href="http://www.softpedia.com/get/Office-tools/Diary-Organizers-Calendar/Agender.shtml">The "100% FREE" Softpedia award</a>
-	<br/><a href="http://www.softpedia.com/get/Office-tools/Diary-Organizers-Calendar/Agender.shtml">
-		<img src="softpedia_free_award_f.gif"/><br/></a><br/>
+	<br/><a href="http://www.softpedia.com/get/Office-tools/Diary-Organizers-Calendar/Agender.shtml"><img src="softpedia_free_award_f.gif"/><br/></a>
 	<div class="citado">
 		Agender is a simple and easy to use schedule / calendar utility. It was writed to be small, simple and work on any computer.
 	</div>
 </li>
 <li><a href="http://www.softoxi.com/agender.html">"Agender - free, fast and easy to use note reminder tool for Windows users."</a>
-<br/><a target="_blank" href="http://www.softoxi.com/agender.html">
-	<img src="award.png"/></a>
-<br/>
+<br/><a target="_blank" href="http://www.softoxi.com/agender.html"><img src="award.png"/></a>
 <div class="citado">
 Agender is a light, free utility that allows you to organize all your thoughts comprehensively. Using the
 application is quite straightforward: simply select one of the dates in the calendar, click Add Note and
@@ -38,13 +35,13 @@ Main Features of Agender:<br/>
 <br/><a href="http://www.cdlibre.org/consultar/catalogo/Ofimatica_Agendas-y-calendarios.html">
   <img src="cdlibre_incluidoen.png" width="100" height="30"
 	title="Visita cdlibre.org" />
-</a><br/>
+</a>
 <div class="citado">
 	Agender es un peque&ntilde;o calendario que permite asociar tareas a fechas determinadas.
 </div>
 </li>
 <li><a href="http://www.softsea.com/review/Agender.html">Agender Review and Download, Calendar Software</a>
-<br/><a href="http://www.softsea.com/review/Agender.html"><img src="http://www.softsea.com/images/pro-logo-5stars-1.gif"/></a><br/>
+<br/><a href="http://www.softsea.com/review/Agender.html"><img src="http://www.softsea.com/images/pro-logo-5stars-1.gif"/></a>
 <div class="citado">
 	Agender 1.1.8 is a Calendar Software product from agender.sourceforge.net, get 5 Stars SoftSea Rating, Agender is a cross-plataform schedule/calendar tool. It was writed to be small, simple and work on any computer with any OS. Agender is free software, it is licensed under the GNU General Public License.
 </div>
@@ -52,7 +49,7 @@ Main Features of Agender:<br/>
 <li><a href="http://agender-linux.programas.com/linux/">Agender - Linux </a>
 <br/><a href="http://agender-linux.programas.com/" target="_blank">
 <img src="http://agender-linux.programas.com/linux/badge"/>
-</a><br/>
+</a>
 <div class="citado">
 &iquest;Se te olvidan las tareas que debes realizar, y las reuniones a las que tienes que acudir? &iquest;Y por qu&eacute; no pruebas a instalar en tu ordenador Agender? Este sencillo programa dispone de un sencillo calendario en el que se le pueden ir a&ntilde;adiendo tareas con facilidad.
 Cada vez que se a&ntilde;ada una nueva nota a alguno de los d&iacute;as del calendario, Agender resaltar&aacute; en color verde dicho d&iacute;a en la interfaz, para que de este modo se distingan los d&iacute;as libres de aquellos que est&aacute;n ocupados.
