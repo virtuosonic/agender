9ee4413bf5eda4d828f82aa73840a01d0f2619b8
virtuosonic <virtuosonic@f8c76fb2-cb6b-4cb0-a8c3-d6e765d22dc4>
2010-11-20 07:30:48 +0000

    added google analytics code

--- web/index.php
+++ web/index.php
@@ -81,6 +81,20 @@ else {$page_data = $error404;}
 	<link rel="icon" href="agender32.png"/>
 	<script src="js/prototype.js" type="text/javascript"></script >
 	<script src="js/scriptaculous.js" type="text/javascript"></script >
+	<!--google analytics-->
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-19811940-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 
 <body <?php echo $loadscript ?>>
