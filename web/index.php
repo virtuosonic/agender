0806b1ba15e8befa62f850811a706d532b0130df
virtuosonic <virtuosonic@f8c76fb2-cb6b-4cb0-a8c3-d6e765d22dc4>
2010-11-22 04:21:07 +0000

    added support for http 304

--- web/index.php
+++ web/index.php
@@ -7,7 +7,6 @@ $page = 'Home';
 $Appear='style="display: none"';
 $loadscript = "onload=\"Effect.toggle('container','Appear');Effect.toggle('left_pane','Appear');Effect.toggle('vs_google_adsense','Appear');Effect.toggle('mail','Appear'); return false;\"";
 }
-//$('container').style.display='none';$('left_pane').style.display='none';$('vs_google_adsense').style.display='none';$('mail').style.display='none';
 $active = 'id="active"';
 $inactive = "";
 $Home = $Downloads =$Press = $Screenshots = $About = $Links = $inactive;
@@ -35,33 +34,45 @@ else if ($page == 'Links') {
 	$Links = $active;
 	$title="Links";
 }
-else {
-	$Home = $inactive;
-	$title = $page;
+
+///blablabla
+
+function setLastModified($pagetime)
+{
+    $last_modified=getlastmod();
+    $header_modified=filemtime($pagetime);
+    if($header_modified > $last_modified)
+    {
+        $last_modified=$header_modified;
+    }
+    header('Last-Modified: ' . date("r",$last_modified));
+    return $last_modified;
 }
 
-$vs_google_search = '<div id="vs_google_search_id">
-<form action="http://www.google.com/cse" id="cse-search-box" target="_blank">
-  <div>
-    <input type="hidden" name="cx" value="partner-pub-8829282890738806:o7d14avk586" />
-    <input type="hidden" name="ie" value="ISO-8859-1" />
-    <input type="text" name="q" size="31" />
-    <input type="submit" name="sa" value="Buscar" />
-  </div>
-</form>
-<script type="text/javascript" src="http://www.google.com/cse/brand?form=cse-search-box&amp;lang=es"></script></div>';
-
+function exitIfNotModifiedSince($last_modified)
+{
+    if(array_key_exists("HTTP_IF_MODIFIED_SINCE",$_SERVER))
+    {
+        $if_modified_since=strtotime(preg_replace('/;.*$/','',$_SERVER["HTTP_IF_MODIFIED_SINCE"]));
+        if($if_modified_since >= $last_modified)
+        {
+            header("HTTP/1.1 304 Not Modified");
+            exit();
+        }
+    }
+}
 $error404 = "<center>".
             "<h3>This page cannot be found, <i>try using google</i>.</h3>http error 404".
             "</center>\n";
+$page_name = $page.'.htm';
 
-$page_name = $page.'.php';
+exitIfNotModifiedSince(setLastModified($page_name));
 
-$pages = glob("*.php");
+$pages = glob("*.htm");
 $page_data = '';
 
 if (in_array($page_name, $pages) and file_exists($page_name)){
-    $page_data = '';
+    $page_data = file_get_contents($page_name);
 }
 else {$page_data = $error404;}
 ?>
@@ -69,9 +80,9 @@ else {$page_data = $error404;}
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
-	<meta  name="author" content="Gabriel Espinoza"/>
-	<meta  name="keywords" content="Agender,agender,agenda,schedule,calendar,calendario,wxWidgets,cross platform,free software,windows,linux,opensource"/>
-	<meta  name="description" content="the cross platform schedule tool"/>
+	<meta name="author" content="Gabriel Espinoza"/>
+	<meta name="keywords" content="Agender,agender,agenda,schedule,calendar,calendario,wxWidgets,cross platform,free software,windows,linux,opensource"/>
+	<meta name="description" content="the cross platform schedule tool"/>
 	<meta name="google-site-verification" content="iUBg-Nc16oi8DZuLt_3KwE8exy7fstuQjP7nMNeTSU4" />
 	<title>Agender - <?php echo $title ?></title>
 	<link rel="stylesheet" type="text/css" href="agender.css" media="screen"/>
@@ -81,22 +92,19 @@ else {$page_data = $error404;}
 	<link rel="icon" href="agender32.png"/>
 	<script src="js/prototype.js" type="text/javascript"></script >
 	<script src="js/scriptaculous.js" type="text/javascript"></script >
+	<!--eliminar si google no empieza a funcionar-->
 	<!--google analytics-->
 	<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-19811940-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-19811940-1']);
+	_gaq.push(['_trackPageview']);
+	(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+	</script>
 </head>
-
 <body <?php echo $loadscript ?>>
 <div id="left_pane" <?php echo $Appear ?>>
 	<a href="http://sourceforge.net/projects/agender" >
@@ -135,18 +143,19 @@ else {$page_data = $error404;}
 		<a <?php echo $About ?> href="?page=About">About</a> |
 		<a <?php echo $Links ?> href="?page=Links">Links</a>
 	</div>
-	<?php echo $vs_google_search ?>
+	<!-- google search -->
+	<div id="vs_google_search_id">
+	<form action="http://www.google.com/cse" id="cse-search-box" target="_blank">
+		<div>
+			<input type="hidden" name="cx" value="partner-pub-8829282890738806:o7d14avk586" />
+			<input type="hidden" name="ie" value="ISO-8859-1" />
+			<input type="text" name="q" size="31" />
+			<input type="submit" name="sa" value="Buscar" />
+		</div>
+	</form>
+	<script type="text/javascript" src="http://www.google.com/cse/brand?form=cse-search-box&amp;lang=es"></script></div>
 	<!-- start of contents -->
-	<?php
-		if ($page_data !== '')
-		{
-			echo $page_data;
-		}
-		else
-		{
-			include $page_name;
-		}
-	?>
+	<?php echo $page_data;?>
 	<!-- end of contents -->
 </div>
 <!-- adsense -->
