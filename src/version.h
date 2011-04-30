f07165b69969d9b788be9860ba030d64fbe9c0e1
virtuosonic <22582422+virtuosonic@users.noreply.github.com>
2011-04-30 22:20:52 +0000

    -added revision data to debug report -deleted commented-out code

--- src/version.h
+++ src/version.h
@@ -14,14 +14,14 @@ namespace Agender{
 	static const long REVISION = 0;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 9;
+	static const long BUILDS_COUNT = 10;
 	#define RC_FILEVERSION 2,0,0,0
 	#define RC_FILEVERSION_STRING "2, 0, 0, 0\0"
 	static const char FULLVERSION_STRING[] = "2.0.0.0";
 	
 	//SVN Version
-	static const char SVN_REVISION[] = "289";
-	static const char SVN_DATE[] = "2011-03-09T23:31:57.844282Z";
+	static const char SVN_REVISION[] = "293";
+	static const char SVN_DATE[] = "2011-04-24T08:39:35.340746Z";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 1;
