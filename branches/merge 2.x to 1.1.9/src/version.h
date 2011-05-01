8623f096a78d93c92a1f044733f3be768e13f4e9
virtuosonic <virtuosonic@f8c76fb2-cb6b-4cb0-a8c3-d6e765d22dc4>
2011-05-01 01:21:01 +0000

    fixed an asert

--- branches/merge 2.x to 1.1.9/src/version.h
+++ branches/merge 2.x to 1.1.9/src/version.h
@@ -1,31 +1,31 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-namespace Agender{
-	
-	//Software Status
-	static const char STATUS[] = "Release";
-	static const char STATUS_SHORT[] = "r";
-	
-	//Standard Version Type
-	static const long MAJOR = 2;
-	static const long MINOR = 0;
-	static const long BUILD = 0;
-	static const long REVISION = 0;
-	
-	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 10;
-	#define RC_FILEVERSION 2,0,0,0
-	#define RC_FILEVERSION_STRING "2, 0, 0, 0\0"
-	static const char FULLVERSION_STRING[] = "2.0.0.0";
-	
-	//SVN Version
-	static const char SVN_REVISION[] = "293";
-	static const char SVN_DATE[] = "2011-04-24T08:39:35.340746Z";
-	
-	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 1;
-	
+namespace Agender {
+
+//Software Status
+static const char STATUS[] = "Release";
+static const char STATUS_SHORT[] = "r";
+
+//Standard Version Type
+static const long MAJOR = 2;
+static const long MINOR = 0;
+static const long BUILD = 0;
+static const long REVISION = 0;
+
+//Miscellaneous Version Types
+static const long BUILDS_COUNT = 10;
+#define RC_FILEVERSION 2,0,0,0
+#define RC_FILEVERSION_STRING "2, 0, 0, 0\0"
+static const char FULLVERSION_STRING[] = "2.0.0.0";
+
+//SVN Version
+static const char SVN_REVISION[] = "293";
+static const char SVN_DATE[] = "2011-04-24T08:39:35.340746Z";
+
+//These values are to keep track of your versioning state, don't modify them.
+static const long BUILD_HISTORY = 1;
+
 
 }
 #endif //VERSION_H
